8cac8eb76cee80365ec7781ed71c92a27ca5926e
BrianLee <472563782@qq.com>
2022-05-10 14:42:57 +0800

    fix: Spring初体验_2
    1.通过yml配置文件形式实现单文件的多环境配置

--- 20220510SpringBoot/Demo03SpringBoot_Config/src/main/java/com/woniuxy/Demo03SpringBootConfigApplication.java
+++ 20220510SpringBoot/Demo03SpringBoot_Config/src/main/java/com/woniuxy/Demo03SpringBootConfigApplication.java
@@ -1,5 +1,6 @@
 package com.woniuxy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ public class Demo03SpringBootConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(Demo03SpringBootConfigApplication.class, args);
     }
+
+
 
     @GetMapping("/hello")
     public String hello() {
